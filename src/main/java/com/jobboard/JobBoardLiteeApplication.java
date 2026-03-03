3cbc70092dd7ac8715d7479b52dcbe2848b2484f
YADAVALLI JAYANTH KUMAR <115489547+Jayanthkumar04@users.noreply.github.com>
2026-03-03 17:14:17 +0530

    commitedd

---
+++ src/main/java/com/jobboard/JobBoardLiteeApplication.java
@@ -0,0 +1,13 @@
+package com.jobboard;
+
+Import org.springframework.boot.SpringApplication;
+Import org.springframework.boot.SpringApplication;
+
+@springbootApplication
+public class JobBoardLiteeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JobBoardLiteeApplication.class, args);
+  }
+
+}
